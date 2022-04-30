b7da5ab46abc50d6a85325efe2518810a343c769
sreedhar <sree@sree-corei3>
2022-04-30 20:41:06 +0530

    dist name and app changes

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:alpine
+COPY ./dist/k8s-learning-frontend ./usr/share/nginx/html
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front-end'`, () => {
+  it(`should have as title 'k8s-learning-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front-end');
+    expect(app.title).toEqual('k8s-learning-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('front-end app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('k8s-learning-frontend app is running!');
   });
 });
