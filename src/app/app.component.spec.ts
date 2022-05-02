23dd2cff27c1e1e770ca66f4dc6b9b59f6a9df04
sreedhar <sree@sree-corei3>
2022-05-02 13:57:53 +0530

    template re-write

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'k8s-learning-frontend'`, () => {
+  it(`should have as title 'CoreUI Free Angular Admin Template'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('k8s-learning-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('k8s-learning-frontend app is running!');
+    expect(app.title).toEqual('CoreUI Free Angular Admin Template');
   });
 });
